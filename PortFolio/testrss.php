9ffe68fa4722e98653c681d8bcf91cadb5cad5f2
Maps332 <90769049+Maps332@users.noreply.github.com>
2022-04-13 09:26:23 +0200

    Add files via upload

---
+++ PortFolio/testrss.php
@@ -0,0 +1,5 @@
+<script language="JavaScript" src="http://feed2js.org//feed2js.php?src=https%3A%2F%2Fwww.techniques-ingenieur.fr%2Frss%2Fbase-documentaire%2Fbase-documentaire_nanotechnologies-ti155.xml&chan=y&num=5&au=y&date=y&tz=GMT&targ=y&utf=y&pc=y&html=a"  charset="UTF-8" type="text/javascript"></script>
+
+<noscript>
+<a href="http://feed2js.org//feed2js.php?src=https%3A%2F%2Fwww.techniques-ingenieur.fr%2Frss%2Fbase-documentaire%2Fbase-documentaire_nanotechnologies-ti155.xml&chan=y&num=5&au=y&date=y&tz=GMT&targ=y&utf=y&pc=y&html=y">View RSS feed</a>
+</noscript>
